164b38d368a91359edaabceb88e231c5d3c9bd4e
Alec-Atlas <alec.atlas@kellogg.northwestern.edu>
2021-02-21 16:16:51 -0600

    final upload

--- movies.js
+++ movies.js
@@ -1,69 +1,68 @@
 firebase.auth().onAuthStateChanged(async function (user) {
   let db = firebase.firestore()
-  
+
   if (user) {
 
     db.collection('users').doc(user.uid).set({
-        name: user.displayName,
-        email: user.email
+      name: user.displayName,
+      email: user.email
     })
 
     document.querySelector('.sign-in-or-sign-out').innerHTML = `
-      <p>Welcome! ${user.displayName}</p>
+      <p>Welcome ${user.displayName} !</p>
       <a href="#" class="sign-out text-pink-500 underline">Sign out</a>
       `
     document.querySelector('.sign-out').addEventListener('click', function (event) {
       event.preventDefault
-      firebase.auth().signout()
+      firebase.auth().signOut()
       document.location.href = `movies.html`
     })
-    
 
-  let apiKey = '5fb75a22a5b19cdea717398af48d6d30'
-  let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US`)
-  let json = await response.json()
-  let movies = json.results
-  console.log(movies)
-  
-  for (let i=0; i<movies.length; i++) {
-    let movie = movies[i]
-    let docRef = await db.collection('watched').doc(`${movie.id}`).get()
-    let watchedMovie = docRef.data()
-    let opacityClass = ''
-    if (watchedMovie) {
-      opacityClass = 'opacity-20'
-    }
+    let apiKey = '5fb75a22a5b19cdea717398af48d6d30'
+    let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US`)
+    let json = await response.json()
+    let movies = json.results
+    console.log(movies)
 
-    document.querySelector('.movies').insertAdjacentHTML('beforeend', `
-      <div class="w-1/5 p-4 movie-${movie.id} ${opacityClass}">
+    for (let i = 0; i < movies.length; i++) {
+      let movie = movies[i]
+      let docRef = await db.collection('watched').doc(`${movie.id}-${user.uid}`).get()
+      let watchedMovie = docRef.data()
+      let opacityClass = ''
+      if (watchedMovie) {
+        opacityClass = 'opacity-20'
+      }
+
+      document.querySelector('.movies').insertAdjacentHTML('beforeend', `
+      <div class="w-1/5 p-4 movie-${movie.id}-${user.uid} ${opacityClass}">
         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="w-full">
         <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
       </div>
     `)
 
-    document.querySelector(`.movie-${movie.id}`).addEventListener('click', async function(event) {
-      event.preventDefault()
-      let movieElement = document.querySelector(`.movie-${movie.id}`)
-      movieElement.classList.add('opacity-20')
-      await db.collection('watched').doc(`${movie.id}`).set({})
-    }) 
+      document.querySelector(`.movie-${movie.id}-${user.uid}`).addEventListener('click', async function (event) {
+        event.preventDefault()
+        let movieElement = document.querySelector(`.movie-${movie.id}-${user.uid}`)
+        movieElement.classList.add('opacity-20')
+        await db.collection('watched').doc(`${movie.id}-${user.uid}`).set({})
+      })
+    }
+
+  } else {
+
+    document.querySelector('.movies').classList.add('hidden')
+
+    let ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+    let authUIConfig = {
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      signInSuccessUrl: 'movies.html'
+    }
+
+    ui.start('.sign-in-or-sign-out', authUIConfig)
   }
-
-} else {
- 
-  document.querySelector('.movies').classList.add('hidden')
-
-  let ui = new firebaseui.auth.AuthUI(firebase.auth())
-
-  let authUIConfig = {
-    signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ],
-    signInSuccessUrl: 'movies.html'
-  }
-
-  ui.start('.sign-in-or-sign-out', authUIConfig)
-}
 
 })
 
